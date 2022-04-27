8e4cffa45fd7be7732bd002906241afc4480e576
Yilin Lan <eileen.lan@mail.utoronto.ca>
2022-04-27 10:20:44 +0800

    update abstract

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,32 +1,22 @@
 #### Preamble ####
 # Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Author: Yilin Lan
+# Data: 26 April 2021
+# Contact: eileen.lan@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
+# Just run the rmd file and the data will be sitmulated.
 # Any other information needed?
 
 
 #### Workspace setup ####
 # Use R Projects, not setwd().
-library(haven)
-library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
+# Just run the rmd file and the data will be sitmulated.
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+library(kableExtra)
+library(ggplot2)
+library(tidyverse)
+
          
 
 #### What's next? ####
